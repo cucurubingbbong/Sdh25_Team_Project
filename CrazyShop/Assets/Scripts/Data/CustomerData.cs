8893817689e6fb894fa87e8dcaa9765909f10eea
cucurubingbbong <93061270+cucurubingbbong@users.noreply.github.com>
2025-06-17 09:50:56 +0900

    dd
    dd

--- CrazyShop/Assets/Scripts/Data/CustomerData.cs
+++ CrazyShop/Assets/Scripts/Data/CustomerData.cs
@@ -18,10 +18,8 @@ public class CustomerData : ScriptableObject
         public int amount;
     }
 
-    // 인스펙터에서 편집하기 위한 리스트
     public List<ItemAmount> requiredItemsList = new();
 
-    // 딕셔너리로 변환
     public Dictionary<ItemData, int> requiredItemsDict;
 
     public float feel;
